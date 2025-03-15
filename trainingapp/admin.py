833d40087ce9ad7ad30b6445f4b59b2401a63c46
morrinegitari <morrinegitari@gmail.com>
2025-03-15 15:45:03 +0300

    my comit

---
+++ trainingapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from trainingapp.models import *
+
+
+# Register your models here.
+admin.site.register(Students)
+admin.site.register(Lecturers)
+admin.site.register(Employees)
+admin.site.register(Parents)
+admin.site.register(Contacts)
